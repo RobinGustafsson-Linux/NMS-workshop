9761f8b0c11881fc4db91f024d73b48a2ec467a7
Robin Gustafsson <robgus0711@icloud.com>
2025-10-09 15:31:52 +0200

    Added report for devices with warning status.
    Also made some slight changes, had wrong aliments on report.write, it was on the same line as with open which creates an error.

--- network_report.py
+++ network_report.py
@@ -19,23 +19,30 @@ with open("network_devices.json", "r") as file:
 
     # Generate the report
 with open("network_report.txt", "w", encoding="utf-8") as report:
-    report.write("=" * 80 + "/n")
-    report.write(f"NÄTVERKSRAPPORT - {company}/n")
-    report.write("=" * 80 + "/n")
-    report.write(f"rapportdatum: {datetime.now().strftime('%Y-YY-%m-%d %H:%M')}/n") # current date and time .strftime to format.
-    report.write(f"Datauppdatering: {last_updated}/n/n")
+    report.write("=" * 80 + "\n")
+    report.write(f"NÄTVERKSRAPPORT - {company}\n")
+    report.write("=" * 80 + "\n")
+    report.write(f"rapportdatum: {datetime.now().strftime('%Y-%m-%d %H:%M')}\n") # current date and time .strftime to format
+    report.write(f"Datauppdatering: {last_updated}\n\n")
 
 
 # list comprehension to filter devices, 1 offline devices and 1 with warnings
 
-offline_devices = [device for device in device if device["status"] == "offline"]
-warning_devices = [device for device in devices if device["status"] == "warning"]
+    offline_devices = [device for device in devices if device["status"] == "offline"]
+    warning_devices = [device for device in devices if device["status"] == "warning"]
 
-report.write("ENHETER MED PROBLEM/n")
-report.write("-------------------/n")
+    report.write("ENHETER MED PROBLEM\n")
+    report.write("-------------------\n")
 
 # f strings to format the output
-report.write("Status: Offline/n")
-for device in offline_devices:
-    report.write(f"- {device['hostname']:15} {device['ip_address']:15} {device['type']:12} {device['site']}/n")
+
+    report.write("Status: Offline\n")
+    for device in offline_devices:
+        report.write(f"  {device['hostname']:15} {device['ip_address']:15} {device['type']:12} {device['site']}\n")
+    report.write("\n")
+
+    report.write("Status: WARNING\n")
+    for device in warning_devices:
+        report.write(f"  {device['hostname']:15} {device['ip_address']:15} {device['type']:12} {device['site']} (uptime: {device['uptime_days']} dagar)\n")
+    report.write("\n")
                  
